5f6414101518baba3d05f1de8f37ba8c3186b3a1
unknown <matthewharre@gmail.com>
2021-11-03 05:52:46 -0700

    created new search/sort folder and exercise

--- 2-Lambda_Exercises/lambda.py
+++ 2-Lambda_Exercises/lambda.py
@@ -41,19 +41,3 @@ fib_series = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]],
                                 
 # print(fib_series(30))
 
-def binary_search(item_list,item):
-	first = 0
-	last = len(item_list)-1
-	found = False
-	while( first<=last and not found):
-		mid = (first + last)//2
-		if item_list[mid] == item:
-			found = True
-		else:
-			if item < item_list[mid]:
-				last = mid - 1
-			else:
-				first = mid + 1	
-	return found
-
-print(binary_search([1,2,3,5,8], 6))
