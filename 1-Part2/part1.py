ecaa531dc9b6daecad8b64eb7d94776f1caabfbc
unknown <matthewharre@gmail.com>
2021-11-03 04:25:28 -0700

    added some exercises in part 1 and part 2

--- 1-Part2/part1.py
+++ 1-Part2/part1.py
@@ -65,8 +65,59 @@ def n_copies_of_first_two(s, n):
     s = s[:2] * n
     return s
 
-
 # print(n_copies_of_first_two('p', 3))
 # print(n_copies_of_first_two('abcd', 10))
 
+def test_vowel(s):
+    if s.lower() in ['a','e','i','o','u']:
+        return True
+    return False
 
+# print(test_vowel('U'))
+
+def histogram(items):
+    for n in items:
+        output = ''
+        times = n
+        while( times > 0 ):
+          output += '*'
+          times = times - 1
+        print(output)
+
+# histogram([2, 3, 6, 5])
+
+def list_into_string(l):
+    s = ''
+    for num in l:
+        s += str(num)
+    return s
+
+# print(list_into_string([1, 5, 12, 2]))
+
+def print_even(l):
+    new_l = []
+    for num in l:
+        if num == 237:
+            return new_l
+        if num % 2 == 0:
+            new_l.append(num)
+    return new_l
+
+# numbers = [    
+#     386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 
+#     399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 
+#     815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717, 
+#     958,743, 527
+#     ]
+# print(print_even(numbers))
+
+def print_set(s1,s2):
+    l = []
+    for color in s1:
+        if color not in s2:
+            l.append(color)
+    return set(l)
+
+# c1 = set(["White", "Black", "Red"])
+# c2 = set(["Red", "Green"])
+# print(print_set(c1,c2))
--- 1-Part2/part2.py
+++ 1-Part2/part2.py
@@ -1,4 +1,4 @@
-import random
+import random, pprint, collections
 
 def is_unique(l):
     if len(l) == len(set(l)):
@@ -23,3 +23,37 @@ def remove_nums(l):
         len_list -= 1
 
 # print(remove_nums([10,20,30,40,50,60,70,80,90]))
+
+def three_sum(nums):
+  result = []
+  nums.sort()
+  for i in range(len(nums)-2):
+    if i > 0 and nums[i] == nums[i-1]:
+      continue
+    l, r = i+1, len(nums)-1
+    while l < r:
+      s = nums[i] + nums[l] + nums[r]
+      if s > 0:
+        r -= 1
+      elif s < 0:
+          l += 1
+      else:
+        # found three sum
+        result.append((nums[i], nums[l], nums[r]))
+        # remove duplicates
+        while l < r and nums[l] == nums[l+1]:
+          l+=1
+          while l < r and nums[r] == nums[r-1]:
+            r -= 1
+            l += 1
+            r -= 1
+          return result
+
+# x = [1, -6, 4, 2, -1, 2, 0, -2, 0 ]
+# print(three_sum(x))
+
+file_input = input('File Name: ')
+with open(file_input, 'r') as info:
+  count = collections.Counter(info.read().upper())
+  value = pprint.pformat(count)
+# print(value)
